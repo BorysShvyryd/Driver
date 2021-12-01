405f5509ad528dab7ba9e19016a33c31656724e6
BorysShvyryd <bormanpgg@gmail.com>
2021-12-01 19:19:03 +0200

    save start

---
+++ src/test/java/com/borman/REST/API/SWAGGER/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borman.REST.API.SWAGGER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
